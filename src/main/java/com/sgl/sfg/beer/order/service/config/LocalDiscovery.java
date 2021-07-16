192a1de579ec95b15686117973f8166fcdabd7cd
lokhansn@gmail.com <lokhansn@gmail.com>
2021-07-16 18:13:13 +0530

    adding order service as eureka client

---
+++ src/main/java/com/sgl/sfg/beer/order/service/config/LocalDiscovery.java
@@ -0,0 +1,11 @@
+package com.sgl.sfg.beer.order.service.config;
+
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Profile("local-discovery")
+@Configuration
+@EnableEurekaClient
+public class LocalDiscovery {
+}
